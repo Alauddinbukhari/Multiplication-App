476b9d30e21a4c428da03e19facc8e2997efee88
Syed Alauddin Bukhari <syedalauddin.b@gmail.com>
2023-10-14 22:15:26 -0600

    Make changes to index.html file

---
+++ index.js
@@ -0,0 +1,34 @@
+// var mul = num1*num2
+// var score=0
+// function app(){
+
+// const min = 1;
+// const max = 100;
+
+// var num1 = Math.floor(Math.random() * (max - min + 1)) + min
+// var num2 = Math.floor(Math.random() * (max - min + 1)) + min
+
+
+// var mul = num1*num2
+
+
+//  document.getElementById("span1").innerHTML=num1
+
+
+//  document.getElementById("span2").innerHTML= num2
+// }
+
+function updateScore(){
+    score+=1
+}
+
+
+
+
+//  document.getElementById("button").addEventListener("click", function() {
+//     if (document.getElementById("input")===mul){
+//         score+=1
+
+//     }
+//   });
+  
