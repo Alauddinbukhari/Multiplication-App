6a5505a7d5049e662913a68eb07e1df63f183ad3
Syed Alauddin Bukhari <syedalauddin.b@gmail.com>
2023-10-15 13:09:54 -0600

    updated codebase need to fix score bug

--- index.js
+++ index.js
@@ -1,34 +1,47 @@
-// var mul = num1*num2
-// var score=0
-// function app(){
 
-// const min = 1;
-// const max = 100;
+const scoreEL = document.getElementsByClassName("score")[0];
+var score = JSON.stringify(localStorage.getItem("score"));
 
-// var num1 = Math.floor(Math.random() * (max - min + 1)) + min
-// var num2 = Math.floor(Math.random() * (max - min + 1)) + min
-
-
-// var mul = num1*num2
-
-
-//  document.getElementById("span1").innerHTML=num1
-
-
-//  document.getElementById("span2").innerHTML= num2
-// }
-
-function updateScore(){
-    score+=1
+if(score===null){
+ score = 0;
+ console.log()
 }
 
+scoreEL.innerText = `Score: ${score}`;
 
+const min = 1;
+const max = 10;
+ 
+var num1 = Math.floor(Math.random() * (max - min + 1)) + min;
+var num2 = Math.floor(Math.random() * (max - min + 1)) + min;
+var correctans = num1 * num2;
 
+document.getElementById("question").innerText = `What is ${num1} multiply by ${num2}?`;
 
-//  document.getElementById("button").addEventListener("click", function() {
-//     if (document.getElementById("input")===mul){
-//         score+=1
+const inputEL = document.getElementById("input");
 
-//     }
-//   });
+document.getElementById("form").addEventListener("submit", (event) => {
+  event.preventDefault(); // Prevent form submission
   
+  const userAns = +inputEL.value;
+  
+  if (userAns === correctans) {
+    score += 1;
+    updateLocalStorage()
+  } else {
+    score -= 1;
+    updateLocalStorage
+  }
+  
+  
+  scoreEL.innerText = `Score: ${score}`;
+  setTimeout(()=>{app()},1000)
+  updateLocalStorage()
+
+});
+
+
+
+function updateLocalStorage(){
+    localStorage.setItem("score",JSON.stringify(score))
+}
